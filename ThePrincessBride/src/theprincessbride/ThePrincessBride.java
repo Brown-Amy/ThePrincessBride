7e1026f96195e9611767db12e67bbd1974095b43
Brown-Amy <therocketmom@gmail.com>
2014-10-10 23:23:38 -0700

    I'm hoping this will work:)

---
+++ ThePrincessBride/src/theprincessbride/ThePrincessBride.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package theprincessbride;
+
+import theprincessbride.model.Game;
+
+/**
+ *
+ * @author amybrown
+ */
+public class ThePrincessBride {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Game gameOne = new Game();
+        
+        gameOne.setName("initial");
+        gameOne.setBestTime(10.00);
+        gameOne.setLastSave("today");
+        gameOne.setTotalTime(5.00);
+        
+        String gameInfo = gameOne.toString();
+        System.out.println(gameInfo);
+    }
+    
+}
