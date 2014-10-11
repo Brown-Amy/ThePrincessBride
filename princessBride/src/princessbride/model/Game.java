5896c32ef903d6a3079cfa0216cb17946750ff5d
Brown-Amy <therocketmom@gmail.com>
2014-10-11 09:46:20 -0700

    cleaned up repository added game and map class.

---
+++ princessBride/src/princessbride/model/Game.java
@@ -0,0 +1,91 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package theprincessbride.model;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author amybrown
+ */
+import java.util.Objects;/**
+ *
+ * @author amybrown
+ */
+public class Game implements Serializable{
+    
+    private String lastSave;
+    private double totalTime;
+    private double bestTime;
+
+    public Game() {
+    }
+
+    
+    public String getLastSave() {
+        return lastSave;
+    }
+
+    public void setLastSave(String lastSave) {
+        this.lastSave = lastSave;
+    }
+
+    public double getTotalTime() {
+        return totalTime;
+    }
+
+    public void setTotalTime(double totalTime) {
+        this.totalTime = totalTime;
+    }
+
+    public double getBestTime() {
+        return bestTime;
+    }
+
+    public void setBestTime(double bestTime) {
+        this.bestTime = bestTime;
+    }
+
+    @Override
+    public String toString() {
+        return "Game{" + "lastSave=" + lastSave + ", totalTime=" + totalTime + ", bestTime=" + bestTime + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 11 * hash + Objects.hashCode(this.lastSave);
+        hash = 11 * hash + (int) (Double.doubleToLongBits(this.totalTime) ^ (Double.doubleToLongBits(this.totalTime) >>> 32));
+        hash = 11 * hash + (int) (Double.doubleToLongBits(this.bestTime) ^ (Double.doubleToLongBits(this.bestTime) >>> 32));
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Game other = (Game) obj;
+        if (!Objects.equals(this.lastSave, other.lastSave)) {
+            return false;
+        }
+        if (Double.doubleToLongBits(this.totalTime) != Double.doubleToLongBits(other.totalTime)) {
+            return false;
+        }
+        if (Double.doubleToLongBits(this.bestTime) != Double.doubleToLongBits(other.bestTime)) {
+            return false;
+        }
+        return true;
+    }
+
+
+    }
+    
+    
+    
